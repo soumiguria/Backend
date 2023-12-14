0641a26b5f9d5a2a9676573d3351828efdc18c6b
soumi guria <soumiguria.cse2022@nsec.ac.in>
2023-12-14 19:47:10 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-
-dotenv.config({ path: "./env" });
+import {app} from "./app.js";
+dotenv.config({ path: "./.env" });
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server started on port ${process.env.PORT || 8000}`);
-    })
-    app.on("error", (err) => {
-        console.log("MONGODB connection error ", err);
-        throw error;
+        console.log(`Server started on port ${process.env.PORT}`);
     })
 })
 .catch((err) => {
