abd26d53013df142101f13e238deec665578a76b
soumi guria <soumiguria.cse2022@nsec.ac.in>
2023-12-27 14:46:35 +0530

    bug fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -44,7 +44,7 @@ router
 
 router
     .route('/cover-image')
-    .patch(verifyJWTy, upload.single('/coverImage'), updateUserCoverImage);
+    .patch(verifyJWT, upload.single('/coverImage'), updateUserCoverImage);
 
 router.route('/c/:username').get(verifyJWT, getUserChannelProfile);
 
